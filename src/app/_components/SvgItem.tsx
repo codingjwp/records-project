fed85ca67a9d4b9fb42261dc19d3a6aa04c3a12c
codingjwp <codingjwp@gmail.com>
2023-10-18 18:51:24 +0900

    feat: svg color 추가

--- src/app/_components/SvgItem.tsx
+++ src/app/_components/SvgItem.tsx
@@ -1,17 +1,31 @@
 import styles from './svgitem.module.css';
 
-type SVGID = "cube" | "commit" | "commit_30";
-
+type SVGID = "cube" | "commit" | "commit_30" | "storages";
+type FILL = "rt" | "ft" | "mn";
 interface SvgItemProps {
   wd: number;
   hi: number;
   id: SVGID;
+  color: FILL;
 }
 
-const SvgItem = ({id, wd, hi}: SvgItemProps) => {
+const SvgItem = ({id, wd, hi, color}: SvgItemProps) => {
+  const fillStyle = (color: FILL) => {
+    switch(color) {
+      case 'rt':
+        return styles.bg_root;
+      case 'ft':
+        return styles.bg_font;
+      case 'mn':
+        return styles.bg_main;
+      default :
+        return '';
+    }
+  }
+  const bgStyle = fillStyle(color);
 
   return (
-    <svg width={wd} height={hi} className={[styles.bgcolor, id === "cube" ? styles.main_animation : ""].join(" ")}>
+    <svg width={wd} height={hi} className={[bgStyle, id === "cube" ? styles.main_animation : ""].join(" ")}>
       <use href={"svgs/iconsprite.svg#" + `${id}`} />
     </svg>
   )
