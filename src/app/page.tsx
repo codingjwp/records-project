a9afdde99d02f1b8c503a2fbe11f8124ef6bf999
codingjwp <codingjwp@gmail.com>
2023-10-18 18:53:03 +0900

    feat: graphql 데이터 불러오는 fetch 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,60 @@ import SvgItem from "./_components/SvgItem"
 import styles from './rootPage.module.css';
 import { esdokdo } from '@/app/_fonts/fontType';
 
-export default function Home() {
+async function totalContributionsFetch() {
+  const now = new Date();
+  const year =  new Date(now.getFullYear(), 0, 1, 0, 0, 0).toISOString();
+  const toDay = now.toISOString();
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${process.env.ACESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          viewer {
+            pinnedItems(first: 10) {
+              totalCount
+            }
+            contributionsCollection(from: "${year}" to: "${toDay}") {
+              contributionCalendar {
+                totalContributions
+              }
+            }
+          }
+        }`,
+    }),
+    next: {
+      revalidate: 60
+    }
+  }
+  const response = await fetch(`${process.env.GITHUB_API}`, options)
+  if (!response.ok) throw new Error("fetch error response");
+  const { data } = await response.json();
+  
+  return { 
+    totalPinStorages: data.viewer.pinnedItems.totalCount,
+    totalContributions: data.viewer.contributionsCollection.contributionCalendar.totalContributions
+  };
+}
+
+export default async function Home() {
+  const { totalPinStorages, totalContributions } = await totalContributionsFetch();
+
   return (
     <main className={styles.main_wrap}>
       <div className={styles.inner}>
-        <SvgItem wd={256} hi={256} id="cube" />
+        <SvgItem wd={256} hi={256} id="cube" color="mn"/>
         <h2 className={`${styles.main_title} ${esdokdo.className}`}>나의 일생 중 한 부분입니다.</h2>
+        <div className={styles.description}>
+          <SvgItem wd={36} hi={36} id="storages" color="ft"/>
+          <span className={styles.sub_text}>{totalPinStorages}</span>
+          <SvgItem wd={36} hi={36} id="commit" color="ft"/>
+          <span className={styles.sub_text}>{totalContributions}</span>
+        </div>
       </div>
     </main>
   )
