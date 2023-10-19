00dff6b1c2518d929739c408e1ee7cce76bfe6fc
codingjwp <codingjwp@gmail.com>
2023-10-19 21:01:43 +0900

    feat: layout 삭제 및 page css 작성

--- src/app/storages/page.tsx
+++ src/app/storages/page.tsx
@@ -1,18 +1,21 @@
 import { storageInfo } from '@/app/_api/dataFetch';
+import styles from './stoages.module.css';
 import Card from '../_components/Card';
 
 const Storages = async () => {
   const nodes = await storageInfo();
   return (
-    <div>
+    <main className={styles.storages_wrapper}>
+      <div className={styles.inner}>
       {
         nodes.map((node) => {
           return (
-            <Card {...node} />
+            <Card key={node.databaseId} {...node} />
           )
         })
       }
-    </div>
+      </div>
+    </main>
   )
 }
 
