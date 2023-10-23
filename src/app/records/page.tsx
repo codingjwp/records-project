0236e6b17c1a078560af26a76464806e781eede1
codingjwp <codingjwp@gmail.com>
2023-10-24 08:35:19 +0900

    feat: 경로 records 부분 layout 삭제 및 수정

---
+++ src/app/records/page.tsx
@@ -0,0 +1,19 @@
+import styles from './recordsPage.module.css';
+import { recordsList } from '@/app/_api/serverFetch';
+import Collapsible from '../_components/_sc/Collapsible';
+
+const RecordsPage = async ({searchParams}: {searchParams: {name: string}}) => {
+  const files = await recordsList(Buffer.from(searchParams.name, 'base64').toString('utf-8'));
+
+  return (
+    <main className={styles.records_wrap}>
+      <div className={styles.inner}>
+        {files.map((item) => {
+          return (<Collapsible key={item.oid} {...item} parent={searchParams.name} />);
+        })}
+      </div>
+    </main>
+  )
+}
+
+export default RecordsPage;
