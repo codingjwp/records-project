a037ca956d2aa23f8cceb0fd5a50327c27ef8f90
codingjwp <codingjwp@gmail.com>
2023-10-24 18:24:26 +0900

    feat: pretter 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
-import styles from './rootPage.module.css';
-// import { esdokdo } from '@/app/_fonts/fontType';
-import SvgItem from '@/app/_components/_sc/SvgItem';
-import { totalCountInfo } from './_api/serverFetch';
+import styles from './rootPage.module.css'
+import SvgItem from '@/app/_components/_sc/SvgItem'
+import { totalCountInfo } from './_api/serverFetch'
 
-export const revalidate = 86400;
+export const revalidate = 86400
 
 export default async function Home() {
-  const { totalPinStorages, totalContributions} = await totalCountInfo();
+  const { totalPinStorages, totalContributions } = await totalCountInfo()
 
   return (
     <main className={styles.main_wrap}>
       <div className={styles.inner}>
-        <SvgItem size='lr' id="cube" color="mn" />
+        <SvgItem size="lr" id="cube" color="mn" />
         <h2 className={styles.main_title}>나의 일생 중 한 부분입니다.</h2>
         <div className={styles.description}>
-          <SvgItem size='md' id="storages" color="ft" />
+          <SvgItem size="md" id="storages" color="ft" />
           <span className={styles.sub_text}>{totalPinStorages}</span>
-          <SvgItem size='md' id="commit" color="ft" />
+          <SvgItem size="md" id="commit" color="ft" />
           <span className={styles.sub_text}>{totalContributions}</span>
         </div>
       </div>
