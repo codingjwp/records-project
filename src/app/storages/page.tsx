37f7ec26a177eec0252a139409eca0fae16ae647
codingjwp <codingjwp@gmail.com>
2023-10-24 18:24:12 +0900

    feat: prtter 수정

--- src/app/storages/page.tsx
+++ src/app/storages/page.tsx
@@ -1,22 +1,18 @@
-import { storageInfo } from '@/app/_api/serverFetch';
-import styles from './stoages.module.css';
-import Card from '../_components/_sc/Card';
+import { storageInfo } from '@/app/_api/serverFetch'
+import styles from './stoages.module.css'
+import Card from '../_components/_sc/Card'
 
 const Storages = async () => {
-  const storages = await storageInfo();
+  const storages = await storageInfo()
   return (
     <main className={styles.storages_wrapper}>
       <div className={styles.inner}>
-      {
-        storages.map((storage) => {
-          return (
-            <Card key={storage.databaseId} {...storage} />
-          )
-        })
-      }
+        {storages.map((storage) => {
+          return <Card key={storage.databaseId} {...storage} />
+        })}
       </div>
     </main>
   )
 }
 
-export default Storages;
+export default Storages
