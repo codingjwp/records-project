8bec93493bf3f882def4a703cc951e0f98385c06
codingjwp <codingjwp@gmail.com>
2023-10-24 18:23:59 +0900

    feat: pretter 수정

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -1,19 +1,27 @@
-import styles from './recordsPage.module.css';
-import { recordsList } from '@/app/_api/serverFetch';
-import Collapsible from '../_components/_sc/Collapsible';
+import styles from './recordsPage.module.css'
+import { recordsList } from '@/app/_api/serverFetch'
+import Collapsible from '../_components/_sc/Collapsible'
 
-const RecordsPage = async ({searchParams}: {searchParams: {name: string}}) => {
-  const files = await recordsList(Buffer.from(searchParams.name, 'base64').toString('utf-8'));
+const RecordsPage = async ({
+  searchParams,
+}: {
+  searchParams: { name: string }
+}) => {
+  const files = await recordsList(
+    Buffer.from(searchParams.name, 'base64').toString('utf-8'),
+  )
 
   return (
     <main className={styles.records_wrap}>
       <div className={styles.inner}>
         {files.map((item) => {
-          return (<Collapsible key={item.oid} {...item} parent={searchParams.name} />);
+          return (
+            <Collapsible key={item.oid} {...item} parent={searchParams.name} />
+          )
         })}
       </div>
     </main>
   )
 }
 
-export default RecordsPage;
+export default RecordsPage
