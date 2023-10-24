c515f093a11dfa2b2630ee79282a93bb38d2e1fd
codingjwp <codingjwp@gmail.com>
2023-10-24 18:22:17 +0900

    feat: markdown of html

---
+++ src/app/_api/transMdOfHtml.ts
@@ -0,0 +1,18 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
+
+export async function transMdOfHtml(text: string) {
+  const res = await unified()
+    .use(remarkParse, { fragment: true })
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
+    .process(text)
+
+  return res.value
+}
