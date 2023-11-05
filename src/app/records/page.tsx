ac5aae05a4c6484816e002e7fb5d08722c7bfbf9
codingjwp <codingjwp@gmail.com>
2023-11-06 06:59:50 +0900

    feat: 최상위 폴더 이동, 상위 폴더 이동 추가

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -2,6 +2,7 @@ import styles from './recordsPage.module.css'
 import { recordsList } from '@/app/_api/serverFetch'
 import Collapsible from '../_components/_sc/Collapsible'
 import { notFound } from 'next/navigation'
+import Link from 'next/link'
 
 export const revalidate = 86400
 
@@ -10,14 +11,29 @@ const RecordsPage = async ({
 }: {
   searchParams: { paths: string }
 }) => {
-  const files = await recordsList(
-    Buffer.from(searchParams.paths, 'base64').toString('utf-8'),
-  )
+  const returnPaths = Buffer.from(searchParams.paths, 'base64').toString('utf-8')
+  const files = await recordsList(returnPaths)
+  const pathsSplit = returnPaths.split('/');
   if (!files) notFound()
-
   return (
     <main className={styles.records_wrap}>
       <div className={styles.inner}>
+        {pathsSplit.length > 1 && 
+          <>
+            <div className={styles.returnlink_wrap}>
+              <Link href={{
+                pathname: 'records',
+                query: { paths: Buffer.from(pathsSplit[0], 'utf-8').toString('base64')}}}
+                className={styles.returnlink}
+                ><span>•</span></Link></div>
+            <div className={styles.returnlink_wrap}>
+              <Link href={{
+              pathname: 'records',
+              query: { paths: Buffer.from(pathsSplit[pathsSplit.length - 2], 'utf-8').toString('base64')}}}
+              className={styles.returnlink}
+              ><span>•••</span></Link></div>
+          </>
+        }
         {files.map((item) => {
           return (
             <Collapsible key={item.oid} {...item} parent={Buffer.from(item.path, 'utf-8').toString('base64')} />
