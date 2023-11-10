cfc45569474a7d23fdc9bb4a4abbf750d312bbc1
codingjwp <codingjwp@gmail.com>
2023-11-10 13:56:37 +0900

    feat: layout 수정

--- src/app/markdown/layout.tsx
+++ src/app/markdown/layout.tsx
@@ -1,7 +1,5 @@
 import styles from './mdLayout.module.css'
 
-export const revalidate = 86400
-
 const MarkdownLayout = ({ children }: { children: React.ReactNode }) => {
   return <main className={styles.markdown}>{children}</main>
 }
