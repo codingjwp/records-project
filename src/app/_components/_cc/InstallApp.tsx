fb9588d915747d7dbbe23fbe163db2c748046dad
codingjwp <codingjwp@gmail.com>
2023-11-11 20:51:24 +0900

    feat: PWA prompt 생성 컴포넌스 만들기

---
+++ src/app/_components/_cc/InstallApp.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect } from "react";
+
+const InstallApp = () => {
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default InstallApp;
